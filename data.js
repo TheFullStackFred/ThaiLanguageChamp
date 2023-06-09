6d10ba219aaee45cfdd4856301375cd8a1abee26
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-06-09 11:29:37 +0200

    Define type as module and make structure on data array

--- data.js
+++ data.js
@@ -1,1 +1,12 @@
-export const data = []
+export const data = [
+  {
+    question: 'What is the polite particle for male speaker?',
+    options: ['Krap', 'Krep', 'Ka', 'Krop'],
+    answer: 'Krap'
+  },
+  {
+    question: 'What is the polite particle for female speaker?',
+    options: ['Krip', 'Ka', 'Krap', 'Krop'],
+    answer: 'Ka'
+  }
+]
