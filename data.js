0f9a7aa5fe8490f58599153001926dfa6e13a307
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-06-10 16:39:46 +0200

    More data

--- data.js
+++ data.js
@@ -33,5 +33,96 @@ export const data = [
     question: "How to say you're welcome or my pleasure?",
     options: ['Chai', 'Yin dii', 'Dai', 'Mai'],
     answer: 'Yin dii'
+  },
+  {
+    question: 'How to say sorry or excuse me?',
+    options: ['Kraw-leo', 'Kaw-toot', 'Kaw-mai', 'Kaw-krap'],
+    answer: 'Kaw-toot'
+  },
+  {
+    question: "How to say it's ok or no problem?",
+    options: ['Mai bpen rai', 'Mai leo', 'Mai bpen chai', 'Krap'],
+    answer: 'Mai bpen rai'
+  },
+  {
+    question: 'How to say how are you?',
+    options: ['Mai bpen rai', 'Mai chai', 'Sa-baai dii mai', 'Sa-baii'],
+    answer: 'Sa-baai dii mai'
+  },
+  {
+    question: 'How to say I am fine?',
+    options: ['Sa-baii dii', 'Sai', 'Sa-baai dii mai', 'Sa-baii'],
+    answer: 'Sa-baai dii mai'
+  },
+  {
+    question: 'How to say I am ok?',
+    options: ['Dii Gaw', 'Gaw dii', 'Song', 'Gao'],
+    answer: 'Gaw dii'
+  },
+  {
+    question: 'How to say I am not feeling well?',
+    options: ['Mai sa-baai', 'Sa-baai mai', 'Krap', 'Ha'],
+    answer: 'Mai sa-baai'
+  },
+  {
+    question: 'How to say what about you?',
+    options: ['Leko', 'Kun la', 'Meko', 'La kun'],
+    answer: 'Kun la'
+  },
+  {
+    question: 'How to say what about you?',
+    options: ['Leko', 'Kun la', 'Meko', 'La kun'],
+    answer: 'Kun la'
+  },
+
+  {
+    question: 'How to say how is it going?, how is? or how was?',
+    options: ['Bpen yang', 'Bang yang', 'Yang bang', 'Bpen yang ngai baang'],
+    answer: 'Bpen yang ngai baang'
+  },
+  {
+    question: "How to say what did you say? or what's that?",
+    options: ['Na-roi', 'Aroi', 'Na arai', 'A-rai na'],
+    answer: 'A-rai na'
+  },
+  {
+    question: "How to say I don't know",
+    options: ['Arai', 'Ruu mai', 'Na ruu', 'Mai ruu'],
+    answer: 'Mai ruu'
+  },
+  {
+    question: "How to say I don't understand",
+    options: ['Jaijai', 'Jai-kao', 'Kaokao', 'Mai kao-jai'],
+    answer: 'Mai ruu'
+  },
+  {
+    question: 'How to say I can speak Thai a little bit',
+    options: [
+      'Puut paa-saa tai daai nit noi',
+      'Paa-saa tai yai',
+      'Nit noi',
+      'Mai'
+    ],
+    answer: 'Puut paa-saa tai daai nit noi'
+  },
+  {
+    question: 'How to say can you speak slowly please?',
+    options: ['Chai', 'Noi', 'Puut chaa-chaa noi', 'Chaa-chaa'],
+    answer: 'Puut chaa-chaa noi'
+  },
+  {
+    question: 'How to say how to say that in english?',
+    options: [
+      'Chai',
+      'Ang-rit noi',
+      'Puut paa-saa ang-grit yang-ngai',
+      'Yang noi'
+    ],
+    answer: 'Puut paa-saa ang-grit yang-ngai'
+  },
+  {
+    question: 'How to say how to say see you later?',
+    options: ['Jer', 'Jer gan', 'Gen jer', 'Jer lii'],
+    answer: 'Jer gan'
   }
 ]
