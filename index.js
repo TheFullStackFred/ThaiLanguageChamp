a06cd3f28885902b13944fba7c3492888f6245af
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-06-11 15:18:29 +0200

    Add background color to answers

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 import { data } from './data.js'
 
 function renderQuestion() {
+  const options = document.getElementsByClassName('options')
+
   const randomIndex = Math.floor(Math.random() * data.length)
   const question = data[randomIndex]
 
   const questionHtml = `
-    <div class="main__container">
-      <h1 class="main__question">${question.question}</h1>
- <p class='card'>${question.optionOne}</p>
- <p class='card'>${question.optionTwo}</p>
- <p class='card'>${question.optionThree}</p>
- <p class='card'>${question.optionFour}</p>
-
-    </div>
+<div class="main__container">
+    <h1 class="main__question">${question.question}</h1>
+    <p class="card options">${question.optionOne}</p>
+    <p class="card options">${question.optionTwo}</p>
+    <p class="card options">${question.optionThree}</p>
+    <p class="card options">${question.optionFour}</p>
+</div>
   `
 
   document.getElementById('main').innerHTML = questionHtml
+
+  for (let option of options) {
+    option.addEventListener('click', handleAnswer)
+  }
+}
+
+function handleAnswer(e) {
+  data.forEach(function (question) {
+    if (e.target.textContent === question.answer) {
+      const options = document.getElementsByClassName('options')
+      for (let option of options) {
+        if (e.target.textContent === option.textContent) {
+          option.style.backgroundColor = 'green'
+        } else {
+          option.style.backgroundColor = 'red'
+        }
+      }
+    }
+  })
 }
 
 renderQuestion()
