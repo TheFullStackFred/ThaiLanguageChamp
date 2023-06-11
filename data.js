f523c45988fd54f403a2a7974ad4a013db38fbdf
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-06-11 14:11:43 +0200

    Render question

--- data.js
+++ data.js
@@ -1,128 +1,178 @@
 export const data = [
   {
     question: 'What is the polite particle for male speaker?',
-    options: ['Krap', 'Krep', 'Ka', 'Krop'],
+    optionOne: 'Krap',
+    optionTwo: 'Krep',
+    optionThree: 'Ka',
+    optionFour: 'Krop',
     answer: 'Krap'
   },
   {
     question: 'What is the polite particle for female speaker?',
-    options: ['Krip', 'Ka', 'Krap', 'Krop'],
+    optionOne: 'Krip',
+    optionTwo: 'Ka',
+    optionThree: 'Krap',
+    optionFour: 'Krop',
     answer: 'Ka'
   },
   {
     question: 'How to say I for male speaker?',
-    options: ['Chan', 'Pom', 'Pen', 'Chon'],
+    optionOne: 'Chan',
+    optionTwo: 'Pom',
+    optionThree: 'Pen',
+    optionFour: 'Chon',
     answer: 'Pom'
   },
   {
     question: 'How to say I for female speaker?',
-    options: ['Pen', 'Pom', 'Pin', 'Chon'],
+    optionOne: 'Pen',
+    optionTwo: 'Pom',
+    optionThree: 'Pin',
+    optionFour: 'Chon',
     answer: 'Chan'
   },
   {
     question: 'How to say hello?',
-    options: ['Aloha', 'Sawatdii', 'Sawa', 'Mai'],
+    optionOne: 'Aloha',
+    optionTwo: 'Sawatdii',
+    optionThree: 'Sawa',
+    optionFour: 'Mai',
     answer: 'Sawatdii'
   },
   {
     question: 'How to say thank you?',
-    options: ['Quack kun', 'Kawp kun', 'Gracias', 'Kiitos'],
+    optionOne: 'Quack kun',
+    optionTwo: 'Kawp kun',
+    optionThree: 'Gracias',
+    optionFour: 'Kiitos',
     answer: 'Kawp kun'
   },
   {
     question: "How to say you're welcome or my pleasure?",
-    options: ['Chai', 'Yin dii', 'Dai', 'Mai'],
+    optionOne: 'Chai',
+    optionTwo: 'Yin dii',
+    optionThree: 'Dai',
+    optionFour: 'Mai',
     answer: 'Yin dii'
   },
   {
     question: 'How to say sorry or excuse me?',
-    options: ['Kraw-leo', 'Kaw-toot', 'Kaw-mai', 'Kaw-krap'],
+    optionOne: 'Kraw-leo',
+    optionTwo: 'Kaw-toot',
+    optionThree: 'Kaw-mai',
+    optionFour: 'Kaw-krap',
     answer: 'Kaw-toot'
   },
   {
     question: "How to say it's ok or no problem?",
-    options: ['Mai bpen rai', 'Mai leo', 'Mai bpen chai', 'Krap'],
+    optionOne: 'Mai bpen rai',
+    optionTwo: 'Mai leo',
+    optionThree: 'Mai bpen chai',
+    optionFour: 'Krap',
     answer: 'Mai bpen rai'
   },
   {
     question: 'How to say how are you?',
-    options: ['Mai bpen rai', 'Mai chai', 'Sa-baai dii mai', 'Sa-baii'],
+    optionOne: 'Mai bpen rai',
+    optionTwo: 'Mai chai',
+    optionThree: 'Sa-baai dii mai',
+    optionFour: 'Sa-baii',
     answer: 'Sa-baai dii mai'
   },
   {
     question: 'How to say I am fine?',
-    options: ['Sa-baii dii', 'Sai', 'Sa-baai dii mai', 'Sa-baii'],
+    optionOne: 'Sa-baii dii',
+    optionTwo: 'Sai',
+    optionThree: 'Sa-baai dii mai',
+    optionFour: 'Sa-baii',
     answer: 'Sa-baai dii mai'
   },
   {
     question: 'How to say I am ok?',
-    options: ['Dii Gaw', 'Gaw dii', 'Song', 'Gao'],
+    optionOne: 'Dii Gaw',
+    optionTwo: 'Gaw dii',
+    optionThree: 'Song',
+    optionFour: 'Gao',
     answer: 'Gaw dii'
   },
   {
     question: 'How to say I am not feeling well?',
-    options: ['Mai sa-baai', 'Sa-baai mai', 'Krap', 'Ha'],
+    optionOne: 'Mai sa-baai',
+    optionTwo: 'Sa-baai mai',
+    optionThree: 'Krap',
+    optionFour: 'Ha',
     answer: 'Mai sa-baai'
   },
   {
     question: 'How to say what about you?',
-    options: ['Leko', 'Kun la', 'Meko', 'La kun'],
+    optionOne: 'Leko',
+    optionTwo: 'Kun la',
+    optionThree: 'Meko',
+    optionFour: 'La kun',
     answer: 'Kun la'
   },
-  {
-    question: 'How to say what about you?',
-    options: ['Leko', 'Kun la', 'Meko', 'La kun'],
-    answer: 'Kun la'
-  },
-
   {
     question: 'How to say how is it going?, how is? or how was?',
-    options: ['Bpen yang', 'Bang yang', 'Yang bang', 'Bpen yang ngai baang'],
+    optionOne: 'Bpen yang',
+    optionTwo: 'Bang yang',
+    optionThree: 'Yang bang',
+    optionFour: 'Bpen yang ngai baang',
     answer: 'Bpen yang ngai baang'
   },
   {
     question: "How to say what did you say? or what's that?",
-    options: ['Na-roi', 'Aroi', 'Na arai', 'A-rai na'],
+    optionOne: 'Na-roi',
+    optionTwo: 'Aroi',
+    optionThree: 'Na arai',
+    optionFour: 'A-rai na',
     answer: 'A-rai na'
   },
   {
     question: "How to say I don't know",
-    options: ['Arai', 'Ruu mai', 'Na ruu', 'Mai ruu'],
+    optionOne: 'Arai',
+    optionTwo: 'Ruu mai',
+    optionThree: 'Na ruu',
+    optionFour: 'Mai ruu',
     answer: 'Mai ruu'
   },
   {
     question: "How to say I don't understand",
-    options: ['Jaijai', 'Jai-kao', 'Kaokao', 'Mai kao-jai'],
+    optionOne: 'Jaijai',
+    optionTwo: 'Jai-kao',
+    optionThree: 'Kaokao',
+    optionFour: 'Mai kao-jai',
     answer: 'Mai ruu'
   },
   {
     question: 'How to say I can speak Thai a little bit',
-    options: [
-      'Puut paa-saa tai daai nit noi',
-      'Paa-saa tai yai',
-      'Nit noi',
-      'Mai'
-    ],
+    optionOne: 'Puut paa-saa tai daai nit noi',
+    optionTwo: 'Paa-saa tai yai',
+    optionThree: 'Nit noi',
+    optionFour: 'Mai',
     answer: 'Puut paa-saa tai daai nit noi'
   },
   {
     question: 'How to say can you speak slowly please?',
-    options: ['Chai', 'Noi', 'Puut chaa-chaa noi', 'Chaa-chaa'],
+    optionOne: 'Chai',
+    optionTwo: 'Noi',
+    optionThree: 'Puut chaa-chaa noi',
+    optionFour: 'Chaa-chaa',
     answer: 'Puut chaa-chaa noi'
   },
   {
-    question: 'How to say how to say that in english?',
-    options: [
-      'Chai',
-      'Ang-rit noi',
-      'Puut paa-saa ang-grit yang-ngai',
-      'Yang noi'
-    ],
+    question: 'How to say how to say that in English?',
+    optionOne: 'Chai',
+    optionTwo: 'Ang-rit noi',
+    optionThree: 'Puut paa-saa ang-grit yang-ngai',
+    optionFour: 'Yang noi',
     answer: 'Puut paa-saa ang-grit yang-ngai'
   },
   {
-    question: 'How to say how to say see you later?',
-    options: ['Jer', 'Jer gan', 'Gen jer', 'Jer lii'],
+    question: 'How to say see you later?',
+    optionOne: 'Jer',
+    optionTwo: 'Jer gan',
+    optionThree: 'Gen jer',
+    optionFour: 'Jer lii',
     answer: 'Jer gan'
   }
 ]
--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import { data } from './data.js'
 
-data.forEach(function (question) {
-  console.log(question)
-})
+function renderQuestion() {
+  const randomIndex = Math.floor(Math.random() * data.length)
+  const question = data[randomIndex]
+
+  const questionHtml = `
+    <div class="main__container">
+      <h1 class="main__question">${question.question}</h1>
+ <p class='card'>${question.optionOne}</p>
+ <p class='card'>${question.optionTwo}</p>
+ <p class='card'>${question.optionThree}</p>
+ <p class='card'>${question.optionFour}</p>
+
+    </div>
+  `
+
+  document.getElementById('main').innerHTML = questionHtml
+}
+
+renderQuestion()
